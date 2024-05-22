bd443df282da4c99e3ac7134e33a1e70a7ce4f22
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2024-05-21 23:24:20 -0600

    changes to the structure of the task module

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,34 +1,40 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
   @Get()
   getTasks(): object {
-    return {};
+    return this.tasksService.getAll();
   }
 
-  @Get('task')
-  getTask(): object {
-    return {};
+  @Get(':id')
+  getTask(@Param('id') id): object {
+    return this.tasksService.getOne(id);
   }
 
   @Post()
-  createTask(): object {
-    return {};
+  createTask(@Body() body): object {
+    return this.tasksService.create(body.task);
   }
 
-  @Put()
-  updateTask(): object {
-    return {};
+  @Put(':id')
+  updateTask(@Param('id') id, @Body() body): object {
+    return this.tasksService.update(id, body.task);
   }
 
   @Delete()
-  deleteTask(): string {
-    return 'Task deleted!';
-  }
-
-  @Delete('tasks')
-  deleteTasks(): string {
-    return 'Tasks deleted!';
+  deleteTask(@Param('id') id): void {
+    return this.tasksService.delete(id);
   }
 }
--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,28 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { Task } from 'src/interfaces/tasks.interface';
+
 @Injectable()
 export class TasksService {
-  getTasks(): Task[] {
-    return [
-      {
-        id: '1',
-        userId: '1',
-        statusId: '1',
-        title: 'Task 1',
-        description: 'Task 1 description',
-        createdAt: new Date(),
-      },
-    ];
+  private Tasks: Task[] = [];
+  getAll(): Task[] {
+    return this.Tasks;
   }
 
-  createTask(): Task {
-    return {
-      id: '1',
-      userId: '1',
-      statusId: '1',
-      title: 'Task 1',
-      description: 'Task 1 description',
-      createdAt: new Date(),
-    };
+  getOne(id: string): Task {
+    return this.Tasks.find((task) => task.id === id);
+  }
+
+  create(task: Task): Task {
+    this.Tasks.push(task);
+    return task;
+  }
+
+  updateStatus(id: string, status: Task): Task {
+    const index = this.Tasks.findIndex((task) => task.id === id);
+    this.Tasks[index] = status;
+    return status;
+  }
+
+  update(id: string, task: Task): Task {
+    const index = this.Tasks.findIndex((task) => task.id === id);
+    this.Tasks[index] = task;
+    return task;
+  }
+
+  delete(id: string): void {
+    const index = this.Tasks.findIndex((task) => task.id === id);
+    this.Tasks.splice(index, 1);
   }
 }
